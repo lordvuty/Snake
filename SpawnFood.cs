c9760058860abd84ad9de8d61f1db2a075d5c743
lordvuty <39684072+lordvuty@users.noreply.github.com>
2018-11-13 15:48:04 -0200

    Add files via upload

--- SpawnFood.cs
+++ SpawnFood.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnFood : MonoBehaviour {
+public class SpawnFood : MonoBehaviour , Interface {
 
     public GameObject foodPrefab;
 
